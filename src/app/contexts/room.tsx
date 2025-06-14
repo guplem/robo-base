4ac36782cdd2aff8d8503e7ae4202e7cd2cb8d5c
Guillem Poy <guplem@gmail.com>
2025-06-14 17:47:38 +0200

    removed

--- src/app/contexts/room.tsx
+++
@@ -1,8 +0,0 @@
-import React, { createContext } from 'react';
-
-export const RoomContext: React.Context<
-	[string | null, React.Dispatch<React.SetStateAction<string | null>>]
-> = createContext<[string | null, React.Dispatch<React.SetStateAction<string | null>>]>([
-	null,
-	(): void => {},
-]);
